031babbb2fa2b4987c171cd164964cbe1b65dfe1
timho49 <timho49@gmail.com>
2016-03-15 18:48:24 +0100

    Create loginwidget.php

---
+++ design/widgets/loginwidget.php
@@ -0,0 +1,27 @@
+<h1>Prijavi se</h1>
+<form action="login.php" method="post">
+	<table>
+		<tr>
+			<td>
+				<h6>Uporabniško ime: </h6><input type="text" name="username" class="StyleTxtField">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<h6>Geslo: </h6><input type="password" name="password" class="StyleTxtField">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<input type="submit" id="submit" value="Prijavi se" class="StyleTxtField">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<a href="register.php">Registriraj se!</a>
+			</td>
+			
+		</tr>
+		
+	</table>
+</form>
