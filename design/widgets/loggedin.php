c65ccf788d8532cedd0cb719665b578d6f3a2d95
timho49 <timho49@gmail.com>
2016-03-16 21:47:12 +0100

    Create loggedin.php

---
+++ design/widgets/loggedin.php
@@ -0,0 +1,24 @@
+<h1>Pozdravljen, <?php echo $user_data['ime']; ?>!</h1>
+<table>
+	<tr>
+		<td>
+			
+		</td>
+	</tr>
+	<tr>
+		<td>
+			
+		</td>
+	</tr>
+	<tr>
+		<td>
+			
+		</td>
+	</tr>
+	<tr>
+		<td>
+			
+		</td>
+	</tr>
+	
+</table>
