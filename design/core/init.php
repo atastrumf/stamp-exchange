26f924a5cdd6e0f3da974eccc8332b4d3d81635c
timho49 <timho49@gmail.com>
2016-03-16 21:44:07 +0100

    Update init.php

--- design/core/init.php
+++ design/core/init.php
@@ -1,10 +1,16 @@
 <?php
-
 session_start();
+ob_start();
 
 require 'database/connect.php';
 require 'functions/general.php';
 require 'functions/users.php';
+include 'head.php';
+
+if (logged_in() === true) {
+	$session_user_id = $_SESSION['user_id'];
+	$user_data = user_data($session_user_id, 'uporabnikID', 'uporabnisko_ime', 'geslo', 'email', 'ime', 'priimek');
+}
 
 $errors = array();
 
