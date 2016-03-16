11abaefe49f0934a3f665105f6c8ec2e697eb0cd
timho49 <timho49@gmail.com>
2016-03-16 21:55:45 +0100

    Added files via upload

---
+++ design/dosezki.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<?php 
+	include 'core/init.php';
+?>
+<body>
+	
+	<div id="Holder">
+		<?php include 'header.php'; ?>
+		<div id="Content">
+			
+			<div id="ContentLeft">
+				<div id="PageHeading">
+				
+				<h1>Dosežki</h1>
+
+			</div>
+			</div>
+			<div id="ContentRight">
+				<?php
+					if (logged_in() === true) {
+
+						include 'widgets/loggedin.php';
+					} else {
+
+						include 'widgets/login.php';
+					}
+				?>
+			</div>
+
+		</div>
+	</div>
+
+</body>
+
+
+</html>
