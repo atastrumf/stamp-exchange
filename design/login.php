c89d67b394ca87bcff377fe276ffc30720edd8f0
timho49 <timho49@gmail.com>
2016-03-16 21:51:12 +0100

    Update login.php

--- design/login.php
+++ design/login.php
@@ -1,5 +1,3 @@
-<?php include 'head.php' ?>
-
 <?php 
 	include 'core/init.php';
 
@@ -8,18 +6,44 @@
 		$password = $_POST['password'];
 
 		if (empty($username) || empty($password)) {
-			$errors[] = 'Vnesti moraš uporabniško ime in geslo!';
+			$errors[] = 'vnesti moras uporabnisko ime IN geslo!';
 		}
 		else if (user_exists($username) === false) {
-			$errors[] = 'Uporabniško ime ne obstaja!';
+			$errors[] = 'uporabnisko ime ne obstaja!';
 		}
 		else if (user_active($username) === false) {
-			$errors[] = 'Račun še ni aktiviran. Preveri mail!';
+			$errors[] = 'račun se ni aktiviran. Preveri mail!';
 		} else {
 
-		}
+			if (strlen($password) > 50) {
+				$errors[] = 'geslo je predolgo!';
+			}
 
-		print_r($errors);
+			$login = login($username, $password);
+
+			if ($login === false) {
+				$errors[] = 'uporabnisko ime / geslo ni pravilno. Poskusi znova.';
+			} else {
+				$_SESSION['user_id'] = $login;
+				header('Location: index.php');
+				exit();
+			}
+		}
+	} else {
+		$errors[] = 'podatki niso bili posredovani.';
 	}
 
+	include 'header.php';
+
+	if (empty($errors) === false) {
+		?>
+
+		<h1>Pri poskusu prijave je prišlo do napake: </h1>
+
+		<?php
+
+		echo output_errors($errors);
+	}
 ?>
+
+<a href="index.php">Domov</a>
