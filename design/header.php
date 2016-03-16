2bcc3765312492d54d65cb4bae52d8ce576aaacf
timho49 <timho49@gmail.com>
2016-03-16 21:50:13 +0100

    Update header.php

--- design/header.php
+++ design/header.php
@@ -1,11 +1,16 @@
+<?php 
+	include 'head.php';
+?>
+
 <header>
 	<nav>
 		<ul>
 			<li><a href="index.php">Moje znamke</a></li>
-			<li><a href="#">Menjavanje</a></li>
-			<li><a href="#">Statistika</a></li>
-			<li><a href="#">Moj profil</a></li>
-			<li><a href="#">Dosežki</a></li>
+			<li><a href="menjavanje.php">Menjavanje</a></li>
+			<li><a href="statistika.php">Statistika</a></li>
+			<li><a href="mojprofil.php">Moj profil</a></li>
+			<li><a href="dosezki.php">Dosezki</a></li>
+			<li><a href="logout.php">Odjavi se</a></li>
 		</ul>
 	</nav>
 </header>
