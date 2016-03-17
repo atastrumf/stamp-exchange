c8e1b4aef2a279babeb18316f27ca64f39b088a7
krajncv <vanesa.krajnc@student.um.si>
2016-03-17 12:59:45 +0100

    Update head.php

--- design/head.php
+++ design/head.php
@@ -1,6 +1,7 @@
 <head>
 	<link rel="stylesheet" href="CSS/layout.css" type="text/css">
 	<link rel="stylesheet" href="CSS/menu.css" type="text/css">
+	<link rel="stylesheet" href="CSS/znamka.css" type="text/css">
 
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 
