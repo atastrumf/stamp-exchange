df78249b246c06169d64bc288cbbfb2c1b1fb7aa
krajncv <vanesa.krajnc@student.um.si>
2016-03-17 12:19:38 +0100

    Update connect.php

--- design/core/database/connect.php
+++ design/core/database/connect.php
@@ -1,6 +1,5 @@
 <?php 
 	$connect_error = 'Pardon, samo je prišlo do neke napake.';
-	mysql_connect('localhost', 'root', 'password') or die ($connect_error);
-	mysql_select_db('znamke_db') or die($connect_error);
-
+	$con = mysqli_connect('localhost', 'root', 'password') or die ($connect_error);
+	mysqli_select_db($con, 'znamke_db') or die($connect_error);
 ?>
