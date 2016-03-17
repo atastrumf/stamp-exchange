08369e1924147913b30d5bf56b76b6a5c40afe2d
krajncv <vanesa.krajnc@student.um.si>
2016-03-17 10:27:00 +0100

    Update index.php

--- design/index.php
+++ design/index.php
@@ -2,6 +2,7 @@
 <html>
 <?php 
 	include 'core/init.php';
+	include 'core/database/connect.php';
 ?>
 <body>
 	
@@ -12,9 +13,47 @@
 			<div id="ContentLeft">
 				<div id="PageHeading">
 				
-				<h1>Moje znamke</h1>
+				<h1>Moje znamke</h1>			
 
-			</div>
+				</div>				
+					
+				<tr><td class='meni'>
+				<form method='POST' action='index.php' name='leto' id='leto'>
+				<br><select 'width='200px' form='leto' name='izbiraLeta'>";
+				<?php
+				for ($i = 1991; $i < 2017; $i++) {	
+					if (isset($_POST["izbiraLeta"]) && $i == $_POST["izbiraLeta"])					
+						echo "<option value=".$i." selected='selected'>".$i."</option>";
+					else
+						echo "<option value=".$i.">".$i."</option>";
+				}
+				?>
+				<input id='button' type='submit' name='submit' value='Izberi'></select></td></tr><br>
+				
+				<?php
+				if (isset($_POST["izbiraLeta"])) {
+					$izbiraLeta=$_POST['izbiraLeta'];
+				
+					$query = "SELECT * FROM znamka WHERE YEAR(STR_TO_DATE(datumIzdaje, '%d.%m.%Y')) = ".$izbiraLeta."";
+					$result = $con->query($query);
+					
+					if ($result->num_rows > 0) {
+						echo "<table><tr>";
+						$i = 5;
+						while($row = $result->fetch_assoc()) {
+							
+							if ($i==5) {
+								echo "</tr><tr>";
+								$i = 0;
+							}
+								
+							echo '<td><img src="data:image/jpeg;base64,'.base64_encode( $row['slika'] ).'"/></td>';
+							$i++;
+						}
+						echo "</tr></table>";
+					}
+				}
+				?>
 			</div>
 			<div id="ContentRight">
 				<?php
@@ -24,7 +63,7 @@
 					} else {
 
 						include 'widgets/login.php';
-					}
+					}					
 				?>
 			</div>
 
