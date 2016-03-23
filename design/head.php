17b5295b3b4485f1cf60b8969a0b22f689458730
timho49 <timho49@gmail.com>
2016-03-24 00:57:01 +0100

    Update head.php

--- design/head.php
+++ design/head.php
@@ -3,6 +3,8 @@
 	<link rel="stylesheet" href="CSS/menu.css" type="text/css">
 	<link rel="stylesheet" href="CSS/znamka.css" type="text/css">
 
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 
 	<title>Menjavanje znamk</title>
