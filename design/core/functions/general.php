11b7a3678f70d2ec0537f06e1a8881104435c597
timho49 <timho49@gmail.com>
2016-03-24 13:05:56 +0100

    Update general.php

--- design/core/functions/general.php
+++ design/core/functions/general.php
@@ -1,5 +1,10 @@
 <?php
 
+	function email($prejemnik, $zadeva, $vsebina) {
+
+		mail($prejemnik, $zadeva, $vsebina, 'From: menjavanjeznamk@posta.si');
+	}
+
 	function spucaj_array(&$item) {
 		$item = mysqli_real_escape_string(mysqli_connect('localhost', 'root', 'password', 'znamke_db'), $item);
 	}
