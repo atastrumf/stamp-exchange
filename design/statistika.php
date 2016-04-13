4d5993915290f9cadbbaad56c1d0f779e54c65e6
krajncv <vanesa.krajnc@student.um.si>
2016-04-13 15:20:45 +0200

    Update statistika.php

--- design/statistika.php
+++ design/statistika.php
@@ -3,6 +3,37 @@
 <?php 
 	include 'core/init.php';
 ?>
+<style>
+#myProgress {
+	margin-left: 10px;
+	position: relative;
+	width: 800px;
+	height: 30px;
+	background-color: #ddd;
+	border: 1px solid #666;
+	border-radius: 3px;
+}
+
+#myBar {
+	position: absolute;
+	width: 23%;
+	height: 100%;
+	background-color: #efc74b;
+	border-right: 1px solid #666;
+	border-radius: 3px;
+}
+
+#label {
+	font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
+	padding-left: 11px;
+	width: 800px;
+}
+
+.zbrano {
+	font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
+	font-weight: bold;
+}
+</style>
 <body>
 	
 	<div id="Holder">
@@ -19,6 +50,13 @@
              ?>
 
 		        </div>
+				
+				<br>
+				<div id="myProgress" >
+					<div id="myBar">
+						<div id="label">23% zapolnjeno - 199/848</div>
+					</div>
+				</div>
 
 		        <script type="text/javascript">
 		          
@@ -40,7 +78,7 @@
 		          mysqli_query($con,"SET CHARACTER SET utf8");
 		          mysqli_query($con,"SET COLLATION_CONNECTION='utf8_general_ci'");
 
-
+				  $k = 0;
 		          for ($i = 1991; $i < 2017; $i++) {  
 		            
 		            echo "<div class='letoZnamke' id='" . $i . "'<h2>" . $i . "</h2><br><a href='#' class='back-to-top'>Na vrh</a><hr></div><div class='znamke'>";
@@ -48,7 +86,10 @@
 		            $query = "SELECT * FROM znamka WHERE YEAR(STR_TO_DATE(datumIzdaje, '%d.%m.%Y')) = ".$i."";
 		            $result = $con->query($query);
 
-		            echo "<table><tr>";
+					$stZbranih = array(3, 15, 16, 4, 21, 6, 9, 31, 8, 1, 17, 3, 19, 21, 7, 4, 9, 5, 0, 0, 0, 0, 0, 0, 0, 0);
+					echo "<table><tr><td class='zbrano'>" . $stZbranih[$k] . "/" . $result->num_rows . "</td>";					
+					$k++;
+					
 		            $j = 18;
 		            while($row = $result->fetch_assoc()) {
 		              
@@ -56,7 +97,7 @@
 		                echo "</tr><tr>";
 		                $j = 0;
 		              }
-		                
+					  		                
 		              echo "<td id='ZnamkaSeznam'>
 		              <a href='znamka.php?ID_slika=" . $row['ID_slika'] . "'>
 		              <img class='malaznamka' src='data:image/jpeg;base64,".base64_encode( $row['slika'] )."'/>
